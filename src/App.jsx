6a8081b1f27e3c5bf05da28b4d350b3aaf6d5d9c
icaro <icaro2rodrigues@gmail.com>
2022-09-28 02:10:46 -0300

    correct bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App () {
    <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
-   </Routes>
+   </Routes >
   )
 }
 export default App
