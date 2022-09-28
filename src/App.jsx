0f00c51ec718ca651fee4185021ae65f512e0b8e
icaro <icaro2rodrigues@gmail.com>
2022-09-28 01:49:21 -0300

    correct bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
-import { Login } from './pages/login'
+import { Login } from './pages/Login'
 
 import './App.scss'
 
